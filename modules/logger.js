df4dcd1d41dbb49a8fd5105cc815873dab19e20f
jpfeiffer16 <jpfeiffer@bluemodus.com>
2016-03-29 17:35:49 -0600

    Splitting log files

--- modules/logger.js
+++ modules/logger.js
@@ -1,16 +1,19 @@
-var fs = require('fs');
+var fs = require('fs'),
+    uuid = require('node-uuid');
 
 module.exports = (function() {
   function Logger() {
+    var self = this;
+    this.identifier = uuid.v4();
     this.log = function(value, callback) {
       var valueToAppend = '================ \n' + value + '\n'
-      fs.appendFile('./log.txt', valueToAppend, function(err) {
+      fs.appendFile('./log' + self.identifier +'.txt', valueToAppend, function(err) {
         if (err) throw err;
         if (typeof callback == 'function') callback();
       });
     };
     var now = new Date();
-    fs.appendFile('./log.txt', '\n# Run: ' + now.toLocaleDateString() + ", " + now.toLocaleTimeString() + '\n', function(err) {
+    fs.appendFile('./log' + self.identifier +'.txt', '\n# Run: ' + now.toLocaleDateString() + ", " + now.toLocaleTimeString() + '\n', function(err) {
       if (err) throw err;
     });
   }
