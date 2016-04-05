bb31f77e7d811feea8f35ecf5060e61cb81fc4fb
jpfeiffer16 <jpfeiffer@bluemodus.com>
2016-04-05 16:49:01 -0600

    More ServerManger stuff

--- modules/serverManager.js
+++ modules/serverManager.js
@@ -1,12 +1,15 @@
 module.exports = function(callback) {
   var Loader = require('./loader'),
       blessed = require('blessed'),
-      //program = blessed.program(),
+      program = blessed.program(),
       screen = blessed.screen();
-  //program.clear();
-  //program.alternateBuffer();
-  screen.key(['C-c'], function() {
+  // program.clear();
+  // program.alternateBuffer();
+  screen.key(['q'], function() {
     return process.exit(0);
+  });
+  process.on('SIGINT', function() {
+    console.log('Got SIGINT');
   });
   Loader.getListOffWorlds('./', function(list) {
     var simpleList = ['New World'];   
@@ -18,8 +21,8 @@ module.exports = function(callback) {
     var list = blessed.list({
       top: 'center',
       left: 'center',
-      width: '50%',
-      height: '50%',
+      width: '70%',
+      height: '70%',
       style: {
         fg: 'white',
         bg: 'magenta',
@@ -34,13 +37,117 @@ module.exports = function(callback) {
     screen.append(list);
     list.on('select', function(element, selection) {
       var newWorld = simpleList[selection] == 'New World';
-      screen.destroy();
-      if (typeof callback == 'function') callback(newWorld, simpleList[selection]); 
+      if (newWorld) {
+        var form = blessed.form({
+          parent: screen,
+          keys: true,
+          left: 'center',
+          top: 'center',
+          width: 30,
+          height: 6,
+          bg: 'green',
+          content: 'Enter new world name'
+        });
+        
+        var worldInput = blessed.textbox({
+          parent: form,
+          top: 2,
+          left: 'center',
+          width: '50%',
+          height: 1,
+          mouse: true,
+          keys: true,
+          shrink: true,
+          inputOnFocus: true,
+          name: 'worldInput',
+          style: {
+            fg: 'white',
+            bg: 'magenta',
+            border: {
+              fg: '#ffffff'
+            }
+          }
+        });
+        
+        var submit = blessed.button({
+          parent: form,
+          mouse: true,
+          keys: true,
+          shrink: true,
+          padding: {
+            left: 1,
+            right: 1
+          },
+          left: 3,
+          bottom: 1,
+          name: 'submit',
+          content: 'submit',
+          style: {
+            bg: 'blue',
+            focus: {
+              bg: 'red'
+            },
+            hover: {
+              bg: 'red'
+            }
+          }
+        });
+
+        var cancel = blessed.button({
+          parent: form,
+          mouse: true,
+          keys: true,
+          shrink: true,
+          padding: {
+            left: 1,
+            right: 1
+          },
+          right:3,
+          bottom: 1,
+          name: 'cancel',
+          content: 'cancel',
+          style: {
+            bg: 'blue',
+            focus: {
+              bg: 'red'
+            },
+            hover: {
+              bg: 'red'
+            }
+          }
+        });
+
+        submit.on('press', function() {
+          form.submit();
+        });
+        
+        worldInput.focus();
+
+        cancel.on('press', function() {
+          form.reset();
+          form.destroy();
+          list.focus();
+        });
+
+        form.on('submit', function(data) {
+          form.setContent('Submitted.');
+          screen.render();
+        });
+
+        form.on('reset', function(data) {
+          form.setContent('Canceled.');
+          screen.render();
+        });
+        screen.render();
+      }
+
+      if (typeof callback == 'function' && !newWorld) {
+        screen.destroy();
+        // program.normalBuffer();
+        callback(false, simpleList[selection]);
+      }
     });
     list.focus();
     screen.render();
-    //setInterval(function() {
-    //  screen.render();
-    //}, 300);
   });
 };
